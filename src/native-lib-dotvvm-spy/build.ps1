7498222a5f1b10e5bc79a2e380a0c6ee992217ac
Standa Lukeš <exyi@outlook.cz>
2022-12-07 20:17:50 +0100

    Publish LS with the .NET Native module

--- src/native-lib-dotvvm-spy/build.ps1
+++ src/native-lib-dotvvm-spy/build.ps1
@@ -2,7 +2,7 @@ dotnet publish /p:NativeLib=Shared /p:SelfContained=true -r win-x64 -c Release
 Copy-Item -Force -Verbose ./bin/Release/net7.0/win-x64/native/* bin/
 
 cd node_binding
-Remove-Item -Recurse build
+Remove-Item -Recurse build -ErrorAction Ignore
 
 yarn install
 yarn build
