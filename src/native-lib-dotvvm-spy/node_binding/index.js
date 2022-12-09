0114591d69cf8427818bd2bea17003e35a1301b0
Standa Lukeš <exyi@outlook.cz>
2022-12-09 18:13:45 +0100

    Better hack for pkg + dynamic native libraries
    See the parent commit for description

--- src/native-lib-dotvvm-spy/node_binding/index.js
+++ src/native-lib-dotvvm-spy/node_binding/index.js
@@ -1,15 +1,1 @@
-try {
-    module.exports = require("./build/Release/dotnet-analyzer-lib-node.node");
-} catch (error1) {
-    if (error1.code !== 'MODULE_NOT_FOUND') {
-        throw error1;
-    }
-    try {
-        module.exports = require("./build/Debug/dotnet-analyzer-lib-node.node");
-    } catch (error2) {
-        if (error2.code !== 'MODULE_NOT_FOUND') {
-            throw error2;
-        }
-        throw error1
-    }
-  }
+module.exports = require("./build/hack/node_modules/dotnet-analyzer-lib-node/dotnet-analyzer-lib-node.node");
